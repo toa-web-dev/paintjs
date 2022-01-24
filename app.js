4960b9efddcf89ae9da0ffa4f6ea8a4556abaa2a
gimhyoungsoo <gimhyoungsoo@gmail.com>
2022-01-24 22:52:49 +0900

    단순한 그리기 구현
    색상 변경, 붓 굵기 변경 해야함

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
 
-function stopPainting(){
-    let painting = false; //painting이 true일때만 그림이 그려집니다.
-}
+canvas.width = 400;
+canvas.height = 600;
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;                                                //painting이 true일때만 그림이 그려집니다.
+
+function startPainting(){ painting = true; }
+
+function stopPainting(){ painting = false; }
 
 function onMouseMove(event){
-    // console.log(event);
     const x = event.offsetX;
     const y = event.offsetY;
-    console.log(x,y);
-}
-
-function onMouseDown(event){
-    painting = true;
-    //그림을 그림
-}
-
-function onMouseUp(event){
-    stopPainting()
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    }else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
 if(canvas){ 
-    
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
-    /** canvas가 존재하면 이벤트리스너 생성
-     * canvas.addEventListener("이벤트리스너 이름" , 함수);
-    */
+
+                                                                    /*
+                                                                     * canvas가 존재하면 이벤트리스너 생성
+                                                                     * canvas.addEventListener("이벤트리스너 이름" , 함수);
+                                                                    */
 }
