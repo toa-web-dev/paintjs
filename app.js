8b64a8a687578896b1cd5d4654d7d73f5e0f3501
gimhyoungsoo <gimhyoungsoo@gmail.com>
2022-01-26 23:02:36 +0900

    색깔 변경 기능 추가
    더 알아 볼 점:
    this로 전역변수를 사용하려 했지만 오류발생(주석참고,31)

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("controls_color")
 
 canvas.width = 400;
 canvas.height = 600;
-ctx.strokeStyle = "#2c2c2c";
+ctx.strokeStyle = "#2c2c2c";                //기본 색상
 ctx.lineWidth = 2.5;
 
 let painting = false;                                                //painting이 true일때만 그림이 그려집니다.
@@ -24,14 +25,25 @@ function onMouseMove(event){
     }
 }
 
+function handleColorClick(event){
+    let color = event.target.style.backgroundColor
+    ctx.strokeStyle = color 
+    /**
+     * this.ctx.strokeStyle = color 를 하면
+     * app.js:30 Uncaught TypeError: Cannot set properties of undefined (setting 'strokeStyle')
+     * at HTMLDivElement.handleColorClick
+     * 위의 오류가 나는 이유 알아낼 것
+    */
+}
+
 if(canvas){ 
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
-
-                                                                    /*
-                                                                     * canvas가 존재하면 이벤트리스너 생성
-                                                                     * canvas.addEventListener("이벤트리스너 이름" , 함수);
-                                                                    */
+    /*
+     * canvas가 존재하면 이벤트리스너 생성
+     * canvas.addEventListener("이벤트리스너 이름" , 함수);
+    */
 }
+Array.from(colors).forEach(color => color.addEventListener("click",handleColorClick))
